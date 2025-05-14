80e989dd8f9c0974eed2d431d5531260ec8428db
khelbig <khelbig@gmail.com>
2025-05-14 09:54:09 -0700

    removing unnecessary files in root

--- src/lib/util/get-direction.ts
+++
@@ -1,5 +0,0 @@
-// src/lib/util/get-direction.ts
-export function getDirection(locale: string | undefined): "ltr" | "rtl" {
-    const rtlLocales = ["ar", "he"];
-    return rtlLocales.includes(locale || "") ? "rtl" : "ltr";
-  }
--- src/modules/layout/components/cart-button/index.tsx
+++
@@ -1,9 +0,0 @@
-// src/modules/layout/components/cart-button/index.tsx
-import { retrieveCart } from "@lib/data/cart";
-import CartDropdown from "../cart-dropdown";
-
-export default async function CartButton() {
-  const cart = await retrieveCart().catch(() => null);
-
-  return <CartDropdown cart={cart} />;
-}
