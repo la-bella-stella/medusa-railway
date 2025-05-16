d2d3437c2f8b8a2df833fb112f6010c587419cf8
Kent Helbig <khelbig@gmail.com>
2025-05-16 13:21:21 -0700

    last change and logging

--- backend/src/plugins/universal-fulfillment/src/providers/universal-fulfillment/index.js
+++ backend/src/plugins/universal-fulfillment/src/providers/universal-fulfillment/index.js
@@ -1,37 +1,43 @@
 import { FulfillmentProviderService } from "@medusajs/medusa/dist/services";
 
-  class UniversalFulfillmentProvider extends FulfillmentProviderService {
-    static identifier = "universal-fulfillment";
+class UniversalFulfillmentProvider extends FulfillmentProviderService {
+  static identifier = "universal-fulfillment";
 
-    async getFulfillmentOptions() {
-      return [
-        {
-          id: "universal-shipping",
-          name: "Standard Shipping",
-          price: 1000, // $10 in cents
-        },
-      ];
-    }
-
-    async validateOption(option) {
-      return true;
-    }
-
-    async createFulfillment(data, items, order, fulfillment) {
-      return { success: true };
-    }
-
-    async cancelFulfillment(fulfillment) {
-      return { success: true };
-    }
-
-    async createReturnFulfillment(data) {
-      return { success: true };
-    }
-
-    async getFulfillmentDocuments(data) {
-      return [];
-    }
+  constructor() {
+    super();
+    console.log("UniversalFulfillmentProvider initialized");
   }
 
-  export default UniversalFulfillmentProvider;
+  async getFulfillmentOptions() {
+    console.log("getFulfillmentOptions called");
+    return [
+      {
+        id: "universal-shipping",
+        name: "Standard Shipping",
+        price: 1000 // $10 in cents
+      }
+    ];
+  }
+
+  async validateOption(option) {
+    return true;
+  }
+
+  async createFulfillment(data, items, order, fulfillment) {
+    return { success: true };
+  }
+
+  async cancelFulfillment(fulfillment) {
+    return { success: true };
+  }
+
+  async createReturnFulfillment(data) {
+    return { success: true };
+  }
+
+  async getFulfillmentDocuments(data) {
+    return [];
+  }
+}
+
+export default UniversalFulfillmentProvider;
